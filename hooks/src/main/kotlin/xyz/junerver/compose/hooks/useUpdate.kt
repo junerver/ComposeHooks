aa382fc2c3cadc44f31ed22aa1ec8180c1e17e06
Junerver <junerver@qq.com>
2024-03-08 11:29:45 +0800

    ✨: add new hook : useUpdate

---
+++ hooks/src/main/kotlin/xyz/junerver/compose/hooks/useUpdate.kt
@@ -0,0 +1,19 @@
+package xyz.junerver.compose.hooks
+
+import androidx.compose.runtime.Composable
+import kotlin.random.Random
+
+/**
+ * Description:
+ * @author Junerver
+ * date: 2024/3/8-11:28
+ * Email: junerver@gmail.com
+ * Version: v1.0
+ */
+@Composable
+fun useUpdate(): () -> Unit {
+    val (_, setState) = useState(0.0)
+    return fun() {
+        setState(Random.nextDouble())
+    }
+}
