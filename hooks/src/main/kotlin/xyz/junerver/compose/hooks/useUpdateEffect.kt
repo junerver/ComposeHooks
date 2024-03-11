d9f43d7ca45a22bea7b7ab7cf4ebc44ab982e9a3
Junerver <junerver@qq.com>
2024-03-11 11:30:35 +0800

    ✨: add new hook : useUpdateEffect

---
+++ hooks/src/main/kotlin/xyz/junerver/compose/hooks/useUpdateEffect.kt
@@ -0,0 +1,25 @@
+package xyz.junerver.compose.hooks
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.Composable
+import kotlinx.coroutines.CoroutineScope
+
+/**
+ * Description: [useUpdateEffect] 用法等同于 [useEffect]，但是会忽略首次执行，只在依赖更新时执行。
+ * @author Junerver
+ * date: 2024/3/11-11:19
+ * Email: junerver@gmail.com
+ * Version: v1.0
+ */
+@SuppressLint("ComposableNaming")
+@Composable
+fun useUpdateEffect(vararg deps: Any, block: suspend CoroutineScope.() -> Unit) {
+    val isMounted = useRef(false)
+    useEffect(*deps) {
+        if (!isMounted.current) {
+            isMounted.current = true
+        } else {
+            block()
+        }
+    }
+}
