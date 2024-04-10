9c8b33d921107ed7c4ececafae3f6227d41662a5
Junerver <junerver@qq.com>
2024-04-10 15:54:47 +0800

    ⚡️: refactor: modify visibility modifiers

--- hooks/src/main/kotlin/xyz/junerver/compose/hooks/userequest/utils/cachePromise.kt
+++ hooks/src/main/kotlin/xyz/junerver/compose/hooks/userequest/utils/cachePromise.kt
@@ -10,18 +10,18 @@ import kotlinx.coroutines.Deferred
  * Version: v1.0
  */
 
-val cachePromise: MutableMap<String, Deferred<*>> = mutableMapOf()
+private val cachePromise: MutableMap<String, Deferred<*>> = mutableMapOf()
 
-fun <T> getCachePromise(cacheKey: String) = cachePromise[cacheKey] as? Deferred<T>
+internal fun <T> getCachePromise(cacheKey: String) = cachePromise[cacheKey] as? Deferred<T>
 
-fun setCachePromise(cacheKey: String, promise: Deferred<*>) {
+internal fun setCachePromise(cacheKey: String, promise: Deferred<*>) {
     cachePromise[cacheKey] = promise
 }
 
 /**
  * 如果是添加到缓存中的Deferred则移除该缓存
  */
-suspend fun <T> Deferred<T>.awaitPlus(): T {
+internal suspend fun <T> Deferred<T>.awaitPlus(): T {
     cachePromise.entries.removeIf { it.value == this }
     return this.await()
 }
--- hooks/src/main/kotlin/xyz/junerver/compose/hooks/userequest/utils/cacheSubscribe.kt
+++ hooks/src/main/kotlin/xyz/junerver/compose/hooks/userequest/utils/cacheSubscribe.kt
@@ -12,7 +12,7 @@ typealias CachedDataChangeListener = (data: Data) -> Unit
 /**
  * 缓存回调，key-listener列表，同一个 cacheKey 可以有多个回调函数。
  */
-val listeners: MutableMap<String, MutableList<CachedDataChangeListener>> = mutableMapOf()
+private val listeners: MutableMap<String, MutableList<CachedDataChangeListener>> = mutableMapOf()
 
 /**
  * 用于触发缓存回调的网络请求数据缓存，同时缓存了 [loading]、[error]、[data]
@@ -26,7 +26,7 @@ data class Data(
 /**
  * 触发初测在缓存回调map[listeners]中的回调函数
  */
-fun trigger(key: String, data: Data) {
+internal fun trigger(key: String, data: Data) {
     if (listeners.containsKey(key)) {
         listeners[key]?.forEach { item -> item(data) }
     }
@@ -35,7 +35,7 @@ fun trigger(key: String, data: Data) {
 /**
  * 将 cacheKey-回调函数，注册到监听器列表，返回值是反订阅函数，只要指定返回函数就可以取消订阅
  */
-fun subscribe(key: String, listener: CachedDataChangeListener): () -> Unit {
+internal fun subscribe(key: String, listener: CachedDataChangeListener): () -> Unit {
     if (!listeners.containsKey(key)) {
         listeners[key] = mutableListOf() // 初始化一个列表给这个key
     }
