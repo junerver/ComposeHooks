fb78cc01cfd46c620c52460800727ff9e69f82d2
Junerver <junerver@qq.com>
2024-07-02 08:48:25 +0800

    ⚡️: refactor `usePersistent`; Add new parameters `forceUseMemory`, Use this parameter to force the use of memory for persistence even under a custom Provider

--- hooks/src/main/kotlin/xyz/junerver/compose/hooks/usePersistent.kt
+++ hooks/src/main/kotlin/xyz/junerver/compose/hooks/usePersistent.kt
@@ -5,50 +5,63 @@ import xyz.junerver.kotlin.Tuple3
 import xyz.junerver.kotlin.plus
 
 /*
-  Description:
-  @author Junerver
-  date: 2024/4/9-16:59
+  Description: A persistence hook that is easy to extend. It uses memory for
+  persistence by default. You can pass in the corresponding function yourself
+  to customize the persistence solution.
+
+  Author Junerver
+  Date: 2024/4/9-16:59
   Email: junerver@gmail.com
   Version: v1.0
 */
 
-/**
- * pass in the key to get the persistent object
- */
+/** pass in the key to get the persistent object */
 private typealias PersistentGet = (String, Any) -> Any
 
-/**
- * Pass in the key, persist the object, and perform persistence
- */
+/** Pass in the key, persist the object, and perform persistence */
 private typealias PersistentSave = (String, Any?) -> Unit
 
-/**
- * Perform clear persistent by pass key
- */
+/** Perform clear persistent by pass key */
 private typealias PersistentClear = (String) -> Unit
 
-/**
- * Perform persistent save
- */
+/** Perform persistent save */
 private typealias SaveToPersistent<T> = (T?) -> Unit
 
 /**
- * The final return value of the persistence hook is a tuple like [state,setState]
+ * The final return value of the persistence hook is a tuple like
+ * [state,setState]
  */
 private typealias PersistentHookReturn<T> = Tuple3<T, SaveToPersistent<T>, PersistentClear>
 
 /**
  * By default, [memorySaveMap] is used for memory persistence.
- * [usePersistent] is a lightweight encapsulation, you need to provide your own persistence solution
- * globally through `PersistentContext.Provider`;
+ * [usePersistent] is a lightweight encapsulation, you need to provide your
+ * own persistence solution globally through `PersistentContext.Provider`;
  */
 val PersistentContext =
     createContext<Tuple3<PersistentGet, PersistentSave, PersistentClear>>((::memoryGetPersistent to ::memorySavePersistent) + ::memoryClearPersistent)
 
+internal val InternalMemoryPersistentContext =
+    createContext<Tuple3<PersistentGet, PersistentSave, PersistentClear>>((::memoryGetPersistent to ::memorySavePersistent) + ::memoryClearPersistent)
+
+
+/**
+ * Use persistent
+ *
+ * @param key persistence key
+ * @param defaultValue persistent default values
+ * @param forceUseMemory whether to force the use of memory for persistence
+ * @param T
+ * @return
+ */
 @Suppress("UNCHECKED_CAST")
 @Composable
-fun <T> usePersistent(key: String, defaultValue: T): PersistentHookReturn<T> {
-    val (get, set, clear) = useContext(context = PersistentContext)
+fun <T> usePersistent(
+    key: String,
+    defaultValue: T,
+    forceUseMemory: Boolean = false,
+): PersistentHookReturn<T> {
+    val (get, set, clear) = useContext(context = if (forceUseMemory) InternalMemoryPersistentContext else PersistentContext)
 
     /**
      * Register an observer callback for each component that uses this state,
@@ -73,15 +86,14 @@ fun <T> usePersistent(key: String, defaultValue: T): PersistentHookReturn<T> {
 
 private val memorySaveMap = mutableMapOf<String, Any?>()
 
-/**
- * Callback function when performing persistence operation
- */
+/** Callback function when performing persistence operation */
 private typealias SavePersistentCallback = () -> Unit
 
 private val listener = mutableMapOf<String, MutableList<SavePersistentCallback>>()
 
 /**
- * you should call this function in your [PersistentSave] fun to notify state update
+ * you should call this function in your [PersistentSave] fun to notify
+ * state update
  */
 fun notifyDefaultPersistentObserver(key: String) {
     listener[key]?.takeIf { it.isNotEmpty() }?.forEach { it.invoke() }
