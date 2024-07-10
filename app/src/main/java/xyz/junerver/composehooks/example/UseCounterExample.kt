45ec6d1aca2934a6f9ce7f0a6981bbb973512cf7
Junerver <junerver@qq.com>
2024-07-10 09:13:58 +0800

    ⚡️: optimize the function calling experience when Either is used as a parameter

--- app/src/main/java/xyz/junerver/composehooks/example/UseCounterExample.kt
+++ app/src/main/java/xyz/junerver/composehooks/example/UseCounterExample.kt
@@ -6,6 +6,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import arrow.core.left
+import arrow.core.right
+import xyz.junerver.compose.hooks.invoke
 import xyz.junerver.compose.hooks.optionsOf
 import xyz.junerver.compose.hooks.useCounter
 import xyz.junerver.composehooks.ui.component.TButton
@@ -21,7 +23,7 @@ import xyz.junerver.composehooks.ui.component.TButton
 @Composable
 fun UseCounterExample() {
     val (current, inc, dec, set, reset) = useCounter(
-        initialValue = 100,
+        initialValue = 100, //即使配置了100也不会超出选项的上下限
         options = optionsOf {
             min = 1
             max = 10
@@ -39,7 +41,25 @@ fun UseCounterExample() {
                     dec(1)
                 }
                 TButton(text = "set(3)") {
-                    set(3.left())
+                    /**
+                     * 这里如果你想传入的是数值则使用 [left] 函数， 如果想传入的是函数 则使用 [right] 函数。
+                     * 也可以手动导入[invoke]来优化使用体验。
+                     *
+                     * Here, if you want to pass in a numerical value, use the [left] function.
+                     * If you want to pass in a function, use the [right] function. You
+                     * can also manually import [invoke] to optimize the user experience.
+                     *
+                     * ```
+                     * set(3.left())
+                     * set({value:Int ->
+                     *      value/3
+                     * }.right())
+                     * ```
+                     */
+                    set(3)
+                }
+                TButton(text = "set(/3)") {
+                    set { value: Int -> value / 3 }
                 }
                 TButton(text = "reset()") {
                     reset()
--- hooks/src/main/kotlin/xyz/junerver/compose/hooks/useCounter.kt
+++ hooks/src/main/kotlin/xyz/junerver/compose/hooks/useCounter.kt
@@ -24,10 +24,35 @@ data class CounterOptions internal constructor(
 internal typealias IncFn = (Int) -> Unit
 internal typealias DecFn = (Int) -> Unit
 
+/**
+ * Use counter
+ *
+ * @param initialValue initial value of count
+ * @param options
+ * @return
+ *
+ * 在使用第三个解构值[SetValueFn]时，如果你想传入的是数值则使用 [left] 函数， 如果想传入的是函数 则使用 [right]
+ * 函数，也可以手动导入[invoke]来优化使用体验。
+ *
+ * When using the third destructured value [SetValueFn], if you want to
+ * pass in a numerical value, use the [left] function. If you want to pass
+ * in a function, use the [right] function. You can also manually import
+ * [invoke] to optimize the user experience.
+ *
+ * ```
+ * set(3.left())
+ * set({value:Int ->
+ *      value/3
+ * }.right())
+ * ```
+ */
 @Composable
-fun useCounter(initialValue: Int = 0, options: CounterOptions): Tuple5<Int, IncFn, DecFn, SetValueFn<Either<Int, (Int) -> Int>>, ResetFn> {
+fun useCounter(
+    initialValue: Int = 0,
+    options: CounterOptions,
+): Tuple5<Int, IncFn, DecFn, SetValueFn<Either<Int, (Int) -> Int>>, ResetFn> {
     val (current, setCurrent, getCurrent) = useGetState(getTargetValue(initialValue, options))
-    fun setValue(value: Either<Int, (Int) -> Int>) {
+    val setValue: SetValueFn<Either<Int, (Int) -> Int>> = { value: Either<Int, (Int) -> Int> ->
         val target = value.fold(
             ifLeft = { it },
             ifRight = { it(getCurrent()) }
@@ -36,18 +61,19 @@ fun useCounter(initialValue: Int = 0, options: CounterOptions): Tuple5<Int, IncF
     }
 
     fun inc(delta: Int) {
-        setValue({ c: Int -> c + delta }.right())
+        setValue { c: Int -> c + delta }
     }
 
     fun dec(delta: Int) {
-        setValue({ c: Int -> c - delta }.right())
+        setValue { c: Int -> c - delta }
     }
 
     fun set(value: Either<Int, (Int) -> Int>) {
         setValue(value)
     }
+
     fun reset() {
-        setValue(initialValue.left())
+        setValue(initialValue)
     }
 
     return Tuple5(
@@ -63,3 +89,7 @@ private fun getTargetValue(value: Int, options: CounterOptions): Int {
     val (min, max) = options
     return value.coerceIn(min, max)
 }
+
+operator fun SetValueFn<Either<Int, (Int) -> Int>>.invoke(leftValue: Int) = this(leftValue.left())
+operator fun SetValueFn<Either<Int, (Int) -> Int>>.invoke(rightValue: (Int) -> Int) =
+    this(rightValue.right())
