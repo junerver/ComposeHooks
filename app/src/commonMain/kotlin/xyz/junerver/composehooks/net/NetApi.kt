63e2562be061c733ffa11b2dbd27d33aba890e3f
Junerver <junerver@qq.com>
2024-09-27 10:06:17 +0800

    🐛: fix

--- app/src/commonMain/kotlin/xyz/junerver/composehooks/net/NetApi.kt
+++ app/src/commonMain/kotlin/xyz/junerver/composehooks/net/NetApi.kt
@@ -39,10 +39,10 @@ object NetApi : WebService {
     private val client = HttpClient(CIO) {
         defaultRequest {
             url(BASE_URL)
-            headers {
+            headers.appendAll(headers {
                 append(HttpHeaders.ContentType, ContentType.Application.Json.withCharset(Charsets.UTF_8).toString())
                 append(HttpHeaders.Authorization, "token $acc_token")
-            }
+            })
         }
         install(HttpTimeout) {
             requestTimeoutMillis = 15000L
@@ -61,7 +61,7 @@ object NetApi : WebService {
         }
         install(Logging) {
             logger = Logger.DEFAULT
-            level = LogLevel.INFO
+            level = LogLevel.ALL
         }
     }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
