5b5c210a397fd771ca264168ac4422e7526b1f5e
Junerver <junerver@qq.com>
2024-12-13 08:35:18 +0800

    🐛: fix sample

--- app/src/androidMain/kotlin/xyz/junerver/composehooks/example/UseDeviceInfoExample.kt
+++ app/src/androidMain/kotlin/xyz/junerver/composehooks/example/UseDeviceInfoExample.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import xyz.junerver.compose.hooks.useDisableScreenshot
+import xyz.junerver.compose.hooks.invoke
 import xyz.junerver.compose.hooks.useFlashlight
 import xyz.junerver.compose.hooks.useGetState
 import xyz.junerver.compose.hooks.useScreenBrightness
