46fb71882dc0dc126ac431a4a6f2cdaa7890a8f8
Junerver <junerver@qq.com>
2025-12-26 15:52:25 +0800

    🩹: supplement imported

--- app/src/commonMain/kotlin/xyz/junerver/composehooks/example/UseChatExample.kt
+++ app/src/commonMain/kotlin/xyz/junerver/composehooks/example/UseChatExample.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
